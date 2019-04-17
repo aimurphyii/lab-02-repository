6b7e0ba9a39719d1da2eb449a16d744f5215f7ef
Aileen Murphy <ai.murf2@gmail.com>
2019-04-17 10:05:19 -0700

    mostly working but filter doesnt discriminate between pages though pages render separately

--- js/appb.js
+++ js/appb.js
@@ -12,7 +12,7 @@ function Gallery(horn) {
     }
 }
 
-// once we create the object, we will store them in this array to call through as needed
+// once we create the object, we will store them in this array to call through as needed, but separately for each json page
 
 Gallery.allHorns1 = [];
 Gallery.allHorns2 = [];
@@ -33,10 +33,10 @@ Gallery.prototype.toHtml = function () {
   // we are going to get main from the dom, and inside of main we are going to add in new div elements conatining data from out gallery objects--now with handlebars...
 
   let $hbarTemplate = $('#horns-template').html();
-  console.log('template source', $hbarTemplate);
+  // console.log('template source', $hbarTemplate);
   // the copied html pattern is now the skeleton of our newly created horn item div
   let compiledTemplate = Handlebars.compile($hbarTemplate);
-  console.log(compiledTemplate);
+  // console.log(compiledTemplate);
 
   return compiledTemplate(this);
 }
@@ -45,11 +45,11 @@ Gallery.prototype.toHtml = function () {
 let jp1 = '/data/page-1.json';
 let jp2 = '/data/page-2.json';
 let tab1 = $('#one');
-let tab2 = $('two');
+let tab2 = $('#two');
 
 
 // now we need to get the data to run this 
-Gallery.readJson = (jpage, galleryset, place) => {
+Gallery.readJson = (jpage, galleryset, place, filter) => {
   // we are going to grab a json file to run through our constructor
   $.get(jpage, 'json')
 // s ogo and get that info then...
@@ -62,42 +62,9 @@ Gallery.readJson = (jpage, galleryset, place) => {
     //then we want ot send them to the html
 
     .then(place)
-    .then(Gallery.loadKeywords = (galleryset) => {
-      let filterKeywords = [];
-      $('option').not(':first').remove();
-      galleryset.forEach(horn => {
-          if (!filterKeywords.includes(horn.keyword))
-          filterKeywords.push(horn.keyword);
-      });
-  
-      let filterkeywords = [];
-      filterkeywords.sort();
-  
-      filterKeywords.forEach(keyword => {
-          let optionTag = `<option value = "${keyword}">${keyword}</option>`;
-          $('select').append(optionTag);
-          console.log(filterKeywords);
-      });
-  })
-    .then(Gallery.populateFilter)
-    .then(Gallery.handleFilter = (galleryset) => {
-      $('select').on('change', function () {
-        let $selected = $(this).val();
-        console.log('selected is ', $selected);
-        if ($selected !== 'default') {
-    
-          galleryset.forEach(horn => {
-    
-            if ($selected === horn.keyword) {
-              console.log($selected);
-              $('div').attr("style", "display: none");
-              $(`.${$selected}`).attr("style", "display: block")
-              console.log('here i am at ',horn.keyword, $selected);
-            }
-          });
-        }
-      })
-    })
+    .then(Gallery.loadKeywords)
+    // .then(Gallery.populateFilter)
+    .then(filter)
 }
 
 
@@ -113,45 +80,69 @@ Gallery.loadHorns2 = () => {
 }
 
 
-// Gallery.loadKeywords = () => {
-//     let filterKeywords = [];
-//     $('option').not(':first').remove();
-//     Gallery.allHorns1.forEach(horn => {
-//         if (!filterKeywords.includes(horn.keyword))
-//         filterKeywords.push(horn.keyword);
-//     });
+Gallery.loadKeywords = () => {
+    let filterKeywords = [];
+    $('option').not(':first').remove();
+    Gallery.allHorns1.forEach(horn => {
+        if (!filterKeywords.includes(horn.keyword))
+        filterKeywords.push(horn.keyword);
+    });
+    Gallery.allHorns2.forEach(horn => {
+      if (!filterKeywords.includes(horn.keyword))
+      filterKeywords.push(horn.keyword);
+  });
+    let filterkeywords = [];
+    filterkeywords.sort();
 
-//     let filterkeywords = [];
-//     filterkeywords.sort();
+    filterKeywords.forEach(keyword => {
+        let optionTag = `<option value = "${keyword}">${keyword}</option>`;
+        $('select').append(optionTag);
+        console.log(filterKeywords);
+    });
+}
 
-//     filterKeywords.forEach(keyword => {
-//         let optionTag = `<option value = "${keyword}">${keyword}</option>`;
-//         $('select').append(optionTag);
-//         console.log(filterKeywords);
-//     });
-// }
+Gallery.handleFilter1 = () => {
+  $('select').on('change', function () {
+    let $selected = $(this).val();
+    console.log('selected is ', $selected);
+    if ($selected !== 'default') {
 
-// Gallery.handleFilter = (galleryset) => {
-//   $('select').on('change', function () {
-//     let $selected = $(this).val();
-//     console.log('selected is ', $selected);
-//     if ($selected !== 'default') {
+      Gallery.allHorns1.forEach(horn => {
 
-//       galleryset.forEach(horn => {
+        if ($selected === horn.keyword) {
+          console.log($selected);
+          $('div').attr("style", "display: none");
+          $(`.${$selected}`).attr("style", "display: block")
+          console.log('here i am at ',horn.keyword, $selected);
+        }
+      });
+    }
+  })
+}
 
-//         if ($selected === horn.keyword) {
-//           console.log($selected);
-//           $('div').attr("style", "display: none");
-//           $(`.${$selected}`).attr("style", "display: block")
-//           console.log('here i am at ',horn.keyword, $selected);
-//         }
-//       });
-//     }
-//   })
-// }
+Gallery.handleFilter2 = () => {
+  $('select').on('change', function () {
+    let $selected = $(this).val();
+    console.log('selected is ', $selected);
+    if ($selected !== 'default') {
 
-$(() => Gallery.readJson(jp1, Gallery.allHorns1, Gallery.loadHorns1));
-$(() => Gallery.readJson(jp2, Gallery.allHorns2, Gallery.loadHorns2));
+      Gallery.allHorns2.forEach(horn => {
+
+        if ($selected === horn.keyword) {
+          console.log($selected);
+          $('div').attr("style", "display: none");
+          $(`.${$selected}`).attr("style", "display: block")
+          console.log('here i am at ',horn.keyword, $selected);
+        }
+      });
+    }
+  })
+}
+
+
+$(() => Gallery.readJson(jp1, Gallery.allHorns1, Gallery.loadHorns1, Gallery.handleFilter1));
+
+$(() => Gallery.readJson(jp2, Gallery.allHorns2, Gallery.loadHorns2, Gallery.handleFilter2));
 
 
 // DOM-ready function
